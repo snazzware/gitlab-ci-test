7450db63f2e51591613bc3457dc517e28f7f5926
Josh McKee <josh@snazzware.com>
2016-03-11 10:27:55 -0600

    Add docblock for setBrewStyle

--- Snazzware/Beer.php
+++ Snazzware/Beer.php
@@ -16,6 +16,10 @@ class Beer
         return $this->brewStyle;
     }
 
+    /**
+     * Sets the style of brew.
+     * @param string $brewStyle The style of brew
+     */
     public function setBrewStyle($brewStyle)
     {
         $this->brewStyle = $brewStyle;
